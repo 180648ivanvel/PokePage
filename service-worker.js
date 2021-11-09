7c04cb1ec432eac8402ecff477c1c9a719edee70
IvanVel <54999898+180648ivanvel@users.noreply.github.com>
2021-11-08 20:46:17 -0600

    Update manifest

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,30 @@
-self.addEventListener('install', ()=>{
-    caches.open('cache-10a-pokepage'),
-    then(e => {
-        e.add('index.html');
-        e.add('css/styles.css');
-        e.add('css/poke.css');
-    });
+//create AppShell
+const _cacheName = 'poke@v1-cache-10a_v2';
+self.addEventListener('install', () => {
+    console.log('install');
 
-    self.addEventListener('fetch', (param) => {
-        param.respondWith(
-            caches.match(param.request)
-        )
-    });
+    caches.open('poke@v1-cache-10a_v2').
+        then(e => {
+            e.add('index.html');
+            e.add('css/styles.css');
+            e.add('css/poke.css');
+        });
+});
+
+self.addEventListener('activate', () => {
+    console.log('Se ha activado el serviceworker')
+});
+
+//En respuesta a la red
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open('poke@v1-cache-10a_v2').then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
 });
